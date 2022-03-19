927a809beb319227fc735fef4f277a069b85d38e
Yaswanth963 <53951879+Yaswanth963@users.noreply.github.com>
2022-03-20 01:54:54 +0530

    Time: 7 ms (32.93%), Space: 44.8 MB (14.09%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -2,20 +2,37 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int m= triangle.size();
         int dp[][]= new int[m][m];
-        for(int d[]:dp)
-            Arrays.fill(d,-1);
-        return fun(0,0,m,triangle,dp);   
-    }
-    int fun(int s, int e, int m,List<List<Integer>> al, int dp[][]){
-        if(s== m-1)
-            return al.get(s).get(e);
-        int bott,diag;
-        if(s+1 <m && dp[s+1][e]!=-1)
-            bott = dp[s+1][e];
-        else bott = fun(s+1,e,m,al,dp);
-        if(s+1< m && dp[s+1][e+1]!=-1)
-            diag = dp[s+1][e+1];
-        else diag = fun(s+1,e+1,m,al,dp);
-        return dp[s][e] = al.get(s).get(e) + Math.min(bott,diag);
+        for(int i=m-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                if(i==m-1)
+                    dp[i][j]= triangle.get(i).get(j);
+                else
+                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 }
+
+//Memoization
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int m= triangle.size();
+//         int dp[][]= new int[m][m];
+//         for(int d[]:dp)
+//             Arrays.fill(d,-1);
+//         return fun(0,0,m,triangle,dp);   
+//     }
+//     int fun(int s, int e, int m,List<List<Integer>> al, int dp[][]){
+//         if(s== m-1)
+//             return al.get(s).get(e);
+//         int bott,diag;
+//         if(s+1 <m && dp[s+1][e]!=-1)
+//             bott = dp[s+1][e];
+//         else bott = fun(s+1,e,m,al,dp);
+//         if(s+1< m && dp[s+1][e+1]!=-1)
+//             diag = dp[s+1][e+1];
+//         else diag = fun(s+1,e+1,m,al,dp);
+//         return dp[s][e] = al.get(s).get(e) + Math.min(bott,diag);
+//     }
+// }
