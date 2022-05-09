edd523302cd59e6c4d3cc13ec45d533954ad6c65
Yaswanth963 <53951879+Yaswanth963@users.noreply.github.com>
2022-05-09 22:16:52 +0530

    Time: 1594 ms (7.86%), Space: 41.3 MB (95.52%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -3,29 +3,25 @@ class Solution {
         int len = s.length();
         int dp[]= new int[len];
         Arrays.fill(dp,-1);
-        return fun(0,s,dp,len);
+        return fun(0,s,dp,len)-1;
     }
     
     int fun(int i, String s, int dp[], int n){
         if(i>=n)
             return 0;
-        if(isPalind(s.substring(i,n)))
-            return dp[i]=0;
         if(dp[i]!=-1)
             return dp[i];
         int min =Integer.MAX_VALUE;
         String t="";
         for(int j=i;j<n;j++){
-            t+=s.charAt(j);
-            if(isPalind(t)){
+            if(isPalind(i,j,s)){
                 min = Math.min(min,1+fun(j+1,s,dp,n));
             }
         }
         return dp[i]=min;
     }
     
-    boolean isPalind(String s){
-        int l=0,r=s.length()-1;
+    boolean isPalind(int l, int r,String s){
         while(l<=r){
             if(s.charAt(l)!=s.charAt(r))
                 return false;
