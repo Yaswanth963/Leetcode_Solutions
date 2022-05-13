4694307abca08320d59c33993d9758f84033e66d
Yaswanth963 <53951879+Yaswanth963@users.noreply.github.com>
2022-05-13 09:05:55 +0530

    Time: 2 ms (57.78%), Space: 41.9 MB (99.77%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,64 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root==null)
+            return null;
+        Node node = root;
+        List<Node> al =new ArrayList<>();
+        int height = height(node);
+        int dp[] = new int[height];
+        // dp[0]=1;
+        for(int i=1;i<height;i++)
+            dp[i]=dp[i-1]+(int)Math.pow(2,i);
+        for(int i=1;i<=height;i++)
+            fun(node,al,i);
+        int j=0;
+        for(int i=0;i<height;i++){
+            for(j=(int)Math.pow(2,i)-1;j<dp[i];j++){
+                al.get(j).next=al.get(j+1);
+            }
+            al.get(j).next=null;
+        }
+        return root;
+    }
+    
+    void fun(Node root,List<Node> al, int level){
+        if(root==null)
+            return;
+        if(level==1)
+            al.add(root);
+        else{
+            fun(root.left,al,level-1);
+            fun(root.right,al,level-1);
+        }
+    }
+    
+    int height(Node root){
+        if(root==null)
+            return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+   
+}
