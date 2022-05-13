d8a1bdda742985af830eca88bc7197f8db6c5ec7
Yaswanth963 <53951879+Yaswanth963@users.noreply.github.com>
2022-05-13 14:01:22 +0530

    Time: 0 ms (100.00%), Space: 46 MB (7.27%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -25,24 +25,55 @@ class Solution {
     public Node connect(Node root) {
         if(root==null)
             return root;
-        Queue<Node> q = new LinkedList<>();
-        Node rot = root;
-        q.add(rot);
-        while(!q.isEmpty()){
-            int size = q.size();
-            Node prev =null;
-            while(size-->0){
-                Node pop = q.poll();
-                if(prev!=null)
-                    pop.next=prev;
-                prev=pop;
-                
-                if(prev.right!=null)
-                    q.add(prev.right);
-                if(prev.left!=null)
-                    q.add(prev.left);
+        Node node=root;
+        while(node!=null){
+            //creating dummy node
+            Node dummy = new Node(0);
+            //reference of dummy
+            Node temp = dummy;
+            while(node!=null){
+                if(node.left!=null){
+                    temp.next= node.left;
+                    temp=temp.next;
+                }
+                if(node.right!=null){
+                    temp.next=node.right;
+                    temp=temp.next;
+                }
+                //covering left and right and moving next in current level
+                node=node.next;
             }
+            //assign dummy.next to node since dummy points at starting of prev level
+            node=dummy.next;
         }
         return root;
     }
 }
+
+
+
+// class Solution {
+//     public Node connect(Node root) {
+//         if(root==null)
+//             return root;
+//         Queue<Node> q = new LinkedList<>();
+//         Node rot = root;
+//         q.add(rot);
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             Node prev =null;
+//             while(size-->0){
+//                 Node pop = q.poll();
+//                 if(prev!=null)
+//                     pop.next=prev;
+//                 prev=pop;
+                
+//                 if(prev.right!=null)
+//                     q.add(prev.right);
+//                 if(prev.left!=null)
+//                     q.add(prev.left);
+//             }
+//         }
+//         return root;
+//     }
+// }
