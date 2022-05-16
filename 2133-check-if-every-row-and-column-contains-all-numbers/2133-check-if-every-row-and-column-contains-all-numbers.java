8d32a2d1c48b86497e549a27a020e00f0d3f0aa1
Yaswanth963 <53951879+Yaswanth963@users.noreply.github.com>
2022-05-16 23:00:23 +0530

    Time: 75 ms (20.60%), Space: 86.3 MB (43.38%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean checkValid(int[][] mat) {
         Set<Integer> s1 = new HashSet<>();
-        Set<Integer> s2 = new HashSet<>();
         int m = mat.length;
         for(int i=0;i<m;i++){
             for(int j=0;j<m;j++){
+                if(s1.contains(mat[i][j]) || s1.contains(-mat[j][i]))
+                    return false;
                 s1.add(mat[i][j]);
-                s2.add(mat[j][i]);
+                s1.add(-mat[j][i]);
             }
-            if(s1.size()!=m || s2.size()!=m)
+            if(s1.size()!=2*m)
                 return false;
-            s1.clear();s2.clear();
+            s1.clear();
         }
         return true;
     }
