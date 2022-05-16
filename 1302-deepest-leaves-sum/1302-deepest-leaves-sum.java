88e85f8d061c8605d5780a7c86008e52d6a1020e
Yaswanth963 <53951879+Yaswanth963@users.noreply.github.com>
2022-05-16 22:26:01 +0530

    Time: 3 ms (76.60%), Space: 55.6 MB (59.10%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,26 +14,25 @@
  * }
  */
 class Solution {
-    int maxHeight = Integer.MIN_VALUE;
-    int height = 0;
-    int maxSum = 0;
+    int sum=0;
     public int deepestLeavesSum(TreeNode root) {
-        inorderDFS(root,height);
-        return maxSum;
+        int height = getHeight(root);
+        getLeafSum(root,height);
+        return sum;
     }
-    public void inorderDFS(TreeNode root, int height){
-        if(root == null) return;
-        height += 1;
-        inorderDFS(root.left,height);
-        if(root.left == null && root.right==null){ // Node is leaf
-            if(maxHeight < height){
-                maxHeight = height;
-                maxSum = root.val;
-            }
-            else if(maxHeight == height){
-                maxSum = maxSum + root.val;
-            }
-        }
-        inorderDFS(root.right,height);
+    
+    void getLeafSum(TreeNode root, int h){
+        if(root==null)
+            return;
+        if(h==1)
+            sum+=root.val;
+        getLeafSum(root.left,h-1);
+        getLeafSum(root.right,h-1);
+    }
+    
+    int getHeight(TreeNode root){
+        if(root==null)
+            return 0;
+        return 1+Math.max(getHeight(root.left),getHeight(root.right));
     }
 }
